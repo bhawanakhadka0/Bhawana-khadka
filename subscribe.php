4d5dd6873beee6c5d4d6b7b128f3df14c982d0c3
bhawanakhadka0 <157736698+bhawanakhadka0@users.noreply.github.com>
2024-02-17 16:44:06 -0500

    Add files via upload

---
+++ subscribe.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Subscribe | COMP1002 HTML, CSS, and JS Fundamentals</title>
+		<meta name="author" content="The name of the document’s author to go here" />
+		<meta name="description" content="A concise and accurate summary of the document content is to appear here">
+		<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
+		<link rel="stylesheet" href="css/styles.css" />
+	</head>
+	<style>
+
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  background: brown;
+     
+
+  background-size: cover;
+}
+header {
+  height: 90px;
+  width: 1100px;
+  display: flex;
+  gap: 20%;
+  justify-content: space-around;
+  padding: 1%;
+  color: white;
+  align-items: center;
+}
+
+header img {
+  border-radius: 25px;
+  height: 90%;
+  width: 7%;
+}
+h1 {
+  color: #29df6e;
+  font-family: "Roboto Condensed", sans-serif;
+}
+
+	</style>
+	<body>
+		<!-- Page-level header -->
+		<header>
+      <img src="pizza slices.jpg" alt="logo of pizza" />
+      <h1>Thank you for your order </h1>
+    </header>
+		
+<?php
+// Capture the value of the emailAddress INPUT from the index.php page by defining a PHP variable called '$emailAddress' and setting the value to the email address as entered by the user in the FORM on index.php
+	//$emailAddress = $_GET['emailAddress'];
+// Use the PHP function, echo() to create an HTML paragraph element that confirms the user's email address -->
+//echo('<p>Your email address, '.$emailAddress.' has been added to our list of subscribers.</p>');
+?>
+			</section>
+		</main>
+		<!-- Page-level footer -->
+		<footer>
+			<p><small>© HTML, CSS, and JS Blocks Inc.</small></p>
+		</footer>
+	</body>
+</html>
